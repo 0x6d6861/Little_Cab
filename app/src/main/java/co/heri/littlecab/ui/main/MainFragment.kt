3f46795cc006714999c4c9f12c4ef478474cf6be
Heri Agape <agape@live.fr>
2019-11-11 18:19:40 +0300

    random stuff

--- app/src/main/java/co/heri/littlecab/ui/main/MainFragment.kt
+++ app/src/main/java/co/heri/littlecab/ui/main/MainFragment.kt
@@ -10,11 +10,17 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.viewpager.widget.ViewPager
 import co.heri.littlecab.R
 import co.heri.littlecab.adapters.BottomSheetSliderAdapter
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.tabs.TabLayout
 
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), OnMapReadyCallback {
 
     companion object {
         fun newInstance() = MainFragment()
@@ -22,6 +28,8 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
+    private lateinit var mMap: GoogleMap
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,17 +37,22 @@ class MainFragment : Fragment() {
     ): View {
         val viewLayout = inflater.inflate(R.layout.main_fragment, container, false)
 
-        this.initBottomSheet(viewLayout);
+            this.initBottomSheet(viewLayout);
 
-        val viewPager = viewLayout.findViewById(R.id.pager) as ViewPager
-        viewPager.adapter = BottomSheetSliderAdapter()
+            val viewPager = viewLayout.findViewById(R.id.pager) as ViewPager
+            viewPager.adapter = BottomSheetSliderAdapter()
 
-        val bottomTab = viewLayout.findViewById<TabLayout>(R.id.bottom_tab);
-        bottomTab.setupWithViewPager(viewPager, true)
+            val bottomTab = viewLayout.findViewById<TabLayout>(R.id.bottom_tab);
+            bottomTab.setupWithViewPager(viewPager, true)
 
-        viewLayout.findViewById<View>(R.id.place_picker_search).setOnClickListener {
-            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-        }
+            viewLayout.findViewById<View>(R.id.place_picker_search).setOnClickListener {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            }
+
+//        this.initializeMap()
+
+
+
 
 
         return viewLayout;
@@ -50,7 +63,24 @@ class MainFragment : Fragment() {
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
 
+//        this.initializeMap()
+        /*val mapFragment = fragmentManager?.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this@MainFragment) */
+
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        this.initializeMap()
+    }
+
+    /*private fun initializeMap() {
+        if (mMap == null) {
+            val mapFrag =
+                fragmentManager?.findFragmentById(R.id.map) as SupportMapFragment
+            mapFrag.getMapAsync(this)
+        }
+    }*/
 
     private fun initBottomSheet(viewLayout: View){
 
@@ -62,6 +92,7 @@ class MainFragment : Fragment() {
         place_picker.afterLayout {
             place_picker_height = place_picker.height;
             place_picker.translationY = place_picker_height * -1f;
+
         }
 
         bottomSheetBehavior =
@@ -94,7 +125,14 @@ class MainFragment : Fragment() {
     }
 
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
 
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 
     private fun interpolateValues(fraction: Float, startValue: Float, endValue: Float): Float {
         return startValue + (fraction * (endValue - startValue)).toFloat()
